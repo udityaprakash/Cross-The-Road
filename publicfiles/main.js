641f437148d3e347c68d0908e72b2cbd921aedc1
UDITYA PRAKASH <udityaprakash983@gmail.com>
2022-10-09 14:43:23 +0530

    building online pull

--- publicfiles/main.js
+++ publicfiles/main.js
@@ -1,7 +1,11 @@
 // alert("welcome to Cross the road Game.");
 var gamescreen = document.querySelector("#gamingscreen");
-
+var count=0;
+function divgenerator(){
+    for(let i=0;i<5;i++){
+        count++;
+        gamescreen.innerHTML+="<div>way for cars:"+count+"</div>";
+    }
+}
 function Waygenerator(){
-
-    gamescreen.innerHTML+="<div>way for cars</div>";
 }
