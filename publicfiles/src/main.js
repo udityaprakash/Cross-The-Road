7058045be45fe95a8889b1482a6f0f72b1e74294
UDITYA PRAKASH <udityaprakash983@gmail.com>
2022-10-10 00:32:22 +0530

    game background made with F and B buttons

--- publicfiles/src/main.js
+++ publicfiles/src/main.js
@@ -1,11 +1,19 @@
-// alert("welcome to Cross the road Game.");
-var gamescreen = document.querySelector("#gamingscreen");
+var gamescreen = document.getElementById("gamingscreen");
 var count=0;
-function divgenerator(){
-    for(let i=0;i<5;i++){
-        count++;
-        gamescreen.innerHTML+="<div>way for cars:"+count+"</div>";
+for(let i=0;i<50;i++){
+    count++;
+    gamescreen.innerHTML+='<div>'+count+'</div><div></div>';
+}
+gamescreen.scrollTop = gamescreen.scrollHeight;
+function forwardmovement(){
+    gamescreen.scrollTop-=100;
+    console.log(gamescreen.scrollTop);
+    if(gamescreen.scrollTop < 1000){
+        gamescreen.innerHTML='Wow You have Won the game';
     }
 }
-function Waygenerator(){
+function Backwardmovement(){
+    if(gamescreen.scrollTop != gamescreen.scrollHeight)
+    gamescreen.scrollTop+=100;
+    console.log(gamescreen.scrollTop);
 }
