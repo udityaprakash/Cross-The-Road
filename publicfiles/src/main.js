f23f74fc8eb958a397c49a2b428aed4338c93ce2
Janhavi <janhavi25verma@gmail.com>
2022-10-10 17:42:38 +0530

    random cars placed

--- publicfiles/src/main.js
+++ publicfiles/src/main.js
@@ -1,19 +1,49 @@
 var gamescreen = document.getElementById("gamingscreen");
-var count=0;
-for(let i=0;i<50;i++){
-    count++;
-    gamescreen.innerHTML+='<div>'+count+'</div><div></div>';
+var count = 0;
+var cars = [
+  "../Images/Car1.png",
+  "../Images/car2.png",
+  "../Images/car4.png",
+  "../Images/car5.png",
+];
+document.addEventListener("keypress", (event) => {
+  switch (event.key) {
+    case "w":
+      forwardmovement();
+      break;
+    case "s":
+      Backwardmovement();
+      break;
+    default:
+      break;
+  }
+});
+
+function cargenerator() {
+  var i = Math.floor(Math.random() * 4);
+  return cars[i];
+}
+for (let i = 0; i < 50; i++) {
+  count++;
+  var car = cargenerator();
+  console.log(count, car);
+  gamescreen.innerHTML +=
+    "<div>" +
+    count +
+    '<img class="carimages" src=' +
+    car +
+    "></div><div></div>";
 }
 gamescreen.scrollTop = gamescreen.scrollHeight;
-function forwardmovement(){
-    gamescreen.scrollTop-=100;
-    console.log(gamescreen.scrollTop);
-    if(gamescreen.scrollTop < 1000){
-        gamescreen.innerHTML='Wow You have Won the game';
-    }
+function forwardmovement() {
+  gamescreen.scrollTop -= 100;
+  console.log(gamescreen.scrollTop);
+  if (gamescreen.scrollTop < 1000) {
+    gamescreen.innerHTML = "Wow You have Won the game";
+  }
 }
-function Backwardmovement(){
-    if(gamescreen.scrollTop != gamescreen.scrollHeight)
-    gamescreen.scrollTop+=100;
-    console.log(gamescreen.scrollTop);
+function Backwardmovement() {
+  if (gamescreen.scrollTop != gamescreen.scrollHeight)
+    gamescreen.scrollTop += 100;
+  console.log(gamescreen.scrollTop);
 }
