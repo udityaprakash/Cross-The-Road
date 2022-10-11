5ead45551f8462ab42d39a7c530985e30c04f5dd
UDITYA PRAKASH <udityaprakash983@gmail.com>
2022-10-12 00:34:50 +0530

    game logic build and designs redefined

--- publicfiles/src/main.js
+++ publicfiles/src/main.js
@@ -1,38 +1,53 @@
 var gamescreen = document.getElementById("gamingscreen");
+var consolebtn = document.getElementById("console");
+var crash = new Audio("../sounds/accident.mp3");
+var begin = new Audio("../sounds/beginning.mp3");
+var win = new Audio("../sounds/winning.mp3");
 var count = 0;
-var manindiv=50;
+var me=90;
 var flipdiv = true;
-var cars = [
-  "cars1.png",
-  "car2.png",
-  "car3.png",
-  "car4.png",
-  "car5.png",
-  "car6.png",
-  "car7.png",
-  "car8.png",
-  "car9.png",
-  "car10.png"
-];
+function character1(){
+  window.location.pathname = "./publicfiles/src/game.html";
+  // var person = document.getElementById("character");
+  // console.log(person);
+  // person.innerHTML='<img src="../Images/char1.png" alt="charcter">';
+}
+function character2(){
+  window.location.pathname = "./publicfiles/src/game.html";
+}
+
+var cars = ["cars1.png","car2.png","car3.png","car4.png","car5.png","car6.png","car7.png","car8.png","car9.png","car10.png"];
 document.addEventListener("keypress", (event) => {
-  switch (event.key) {
-    case "w":
-      forwardmovement();
-      break;
-    case "s":
-      Backwardmovement();
-      break;
-    default:
-      break;
+  if(consolebtn.style.display != 'none'){
+    switch (event.key) {
+      case "w":
+        forwardmovement();
+        break;
+      case "s":
+        Backwardmovement();
+        break;
+      default:
+        break;
+    }
+
   }
 });
+document.querySelector('#gamingscreen').addEventListener('wheel', (e)=>{
+  e.preventDefault();
+  e.stopPropagation();
+  return false;
+
+}, {passive: false});
+function welcome(){
+  begin.play();
+}
 
 function cargenerator() {
   var i = Math.floor(Math.random() * 10);
   return cars[i];
 }
 function speedduration(){
-    let s = Math.random() * 5;
+    let s = Math.random() * 3;
     return s+1;
 }
 for (let i = 0; i < 50; i++) {
@@ -47,10 +62,54 @@ gamescreen.scrollTop = gamescreen.scrollHeight;
 function forwardmovement() {
   gamescreen.scrollTop -= 100;
   if (gamescreen.scrollTop < 1000) {
-    gamescreen.innerHTML = '<div id="won">Wow You have Won the game</div>';
+    win.play();
+    gamescreen.innerHTML = '<div id="won">You Won<button id="retrybtn" onclick="relod()">Play Again</button></div>';
+    consolebtn.style.display='none';
+    gamescreen.style.height="100vh";
+    clearInterval(le);
+  }
+  if(flipdiv){
+    me = count;
+    count--;
+    flipdiv = false;
+    
+  }else{
+    me = 90;
+    flipdiv = true;
   }
 }
 function Backwardmovement() {
-  if (gamescreen.scrollTop != gamescreen.scrollHeight)
+  if (gamescreen.scrollTop != gamescreen.scrollHeight){
     gamescreen.scrollTop += 100;
+    if(flipdiv){
+      me = count;
+      flipdiv = false;
+    }else{
+      me = 90;
+      count++;
+      flipdiv = true;
+    }
+  }
 }
+function relod(){
+  window.location.reload();
+  welcome();
+}
+var le = setInterval(()=>{
+  if(me!=90){
+    carposition = document.querySelector("#ima"+me);
+    carpos=parseInt(window.getComputedStyle(carposition,null).getPropertyValue('left'));
+    characterposition = document.querySelector("#character");
+    charpos=parseInt(window.getComputedStyle(characterposition,null).getPropertyValue('left'));
+    let gap = carpos - charpos;
+    console.log(count,me);
+    if(gap<70 && gap > -150){
+      clearInterval(le);
+      crash.play();
+      gamescreen.innerHTML = '<div id="won">Game Over<button id="retrybtn" onclick="relod()">Retry</button></div>';
+      consolebtn.style.display='none';
+      gamescreen.style.height="100vh";
+
+    }
+  }
+},100);
