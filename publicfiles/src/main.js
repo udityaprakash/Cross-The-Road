8b0516ce2ff78ca85d34bc7208121b5902437b10
Janhavi <janhavi25verma@gmail.com>
2022-10-11 23:05:51 +0530

    car image  array

--- publicfiles/src/main.js
+++ publicfiles/src/main.js
@@ -1,11 +1,12 @@
 var gamescreen = document.getElementById("gamingscreen");
 var count = 0;
-var cars = [
-  "../Images/Car1.png",
-  "../Images/car2.png",
-  "../Images/car4.png",
-  "../Images/car5.png",
-];
+var manindiv = 50;
+var flipdiv = true;
+
+var cars = [];
+for (let i = 1; i <= 10; i++) {
+  cars.push(`car${i}.png`);
+}
 document.addEventListener("keypress", (event) => {
   switch (event.key) {
     case "w":
@@ -20,30 +21,37 @@ document.addEventListener("keypress", (event) => {
 });
 
 function cargenerator() {
-  var i = Math.floor(Math.random() * 4);
+  var i = Math.floor(Math.random() * 10);
   return cars[i];
+}
+function speedduration() {
+  let s = Math.random() * 5;
+  return s + 1;
 }
 for (let i = 0; i < 50; i++) {
   count++;
   var car = cargenerator();
-  console.log(count, car);
   gamescreen.innerHTML +=
-    "<div>" +
+    '<div id="dive' +
     count +
-    '<img class="carimages" src=' +
+    '"><img class="carimages" id="ima' +
+    count +
+    '" src="../Images/' +
     car +
-    "></div><div></div>";
+    '"></div><div id="div' +
+    count +
+    '"></div>';
+  var speed = document.getElementById("ima" + count);
+  speed.style.animationDuration = speedduration() + "s";
 }
 gamescreen.scrollTop = gamescreen.scrollHeight;
 function forwardmovement() {
   gamescreen.scrollTop -= 100;
-  console.log(gamescreen.scrollTop);
   if (gamescreen.scrollTop < 1000) {
-    gamescreen.innerHTML = "Wow You have Won the game";
+    gamescreen.innerHTML = '<div id="won">Wow You have Won the game</div>';
   }
 }
 function Backwardmovement() {
   if (gamescreen.scrollTop != gamescreen.scrollHeight)
     gamescreen.scrollTop += 100;
-  console.log(gamescreen.scrollTop);
 }
