2ef97b626223e4fd8aa1e5349b39ec4d6cc8d234
UDITYA PRAKASH <udityaprakash983@gmail.com>
2022-10-11 19:02:25 +0530

    added character with functionality

--- publicfiles/src/main.js
+++ publicfiles/src/main.js
@@ -47,35 +47,10 @@ gamescreen.scrollTop = gamescreen.scrollHeight;
 function forwardmovement() {
   gamescreen.scrollTop -= 100;
   if (gamescreen.scrollTop < 1000) {
-    gamescreen.innerHTML = "Wow You have Won the game";
+    gamescreen.innerHTML = '<div id="won">Wow You have Won the game</div>';
   }
-  addmenindiv();
 }
 function Backwardmovement() {
   if (gamescreen.scrollTop != gamescreen.scrollHeight)
     gamescreen.scrollTop += 100;
 }
-function addmenindiv(){
-  var mangrass = document.getElementById("div"+manindiv);
-  var manroad = document.getElementById("dive"+manindiv);
-  var dinoimage = document.getElementById("dino");
-  if(flipdiv){
-    if(manindiv!=50){
-      dinoimage.innerHTML='';
-    }
-    mangrass.innerHTML+='<center><img id="dino" src="../Images/char1.png" height="80px"></center>';
-    // var manback=document.getElementById("dive"+(manindiv-1));
-    // manback.innerHTML='';
-    flipdiv=false;
-    
-  }else{
-    dinoimage.style.display='none';
-    manroad.innerHTML+='<center><img id="dino" src="../Images/char1.png" height="80px"></center>';
-    dinoimage.style.position='absolute';
-    dinoimage.style.top='-100px';
-    
-    manindiv--;
-    flipdiv=true;
-  }
-}
-addmenindiv();
