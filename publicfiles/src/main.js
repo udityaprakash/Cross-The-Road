94e321257817bfeec12f8154e6ceed2ba7190afc
Janhavi <janhavi25verma@gmail.com>
2022-10-14 01:22:56 +0530

    progress bar

--- publicfiles/src/main.js
+++ publicfiles/src/main.js
@@ -5,16 +5,31 @@ var begin = new Audio("../sounds/beginning.mp3");
 var win = new Audio("../sounds/winning.mp3");
 var maxwidth = window.innerWidth;
 var characterposition = document.querySelector("#character");
-var charpos=parseInt(window.getComputedStyle(characterposition,null).getPropertyValue('left'));
-var y=charpos;
-var scorediv=document.querySelector("#scorecard span");
-var score=0;
+var charpos = parseInt(
+  window.getComputedStyle(characterposition, null).getPropertyValue("left")
+);
+var y = charpos;
+var scorediv = document.querySelector("#scorecard span");
+var progressdiv = document.querySelector("#progress-bar span");
+var score = 0;
 var count = 0;
-var me=90;
+var progress = 50;
+var me = 90;
 var flipdiv = true;
-var cars = ["cars1.png","car2.png","car3.png","car4.png","car5.png","car6.png","car7.png","car8.png","car9.png","car10.png"];
+var cars = [
+  "cars1.png",
+  "car2.png",
+  "car3.png",
+  "car4.png",
+  "car5.png",
+  "car6.png",
+  "car7.png",
+  "car8.png",
+  "car9.png",
+  "car10.png",
+];
 document.addEventListener("keypress", (event) => {
-  if(consolebtn.style.display != 'none'){
+  if (consolebtn.style.display != "none") {
     switch (event.key) {
       case "w":
         forwardmovement();
@@ -23,107 +38,128 @@ document.addEventListener("keypress", (event) => {
         Backwardmovement();
         break;
       case "a":
-          leftmovement();
-          break; 
+        leftmovement();
+        break;
       case "d":
-            rightmovement();
-            break;
+        rightmovement();
+        break;
       default:
         break;
     }
   }
 });
-document.querySelector('#gamingscreen').addEventListener('wheel', (e)=>{
-  e.preventDefault();
-  e.stopPropagation();
-  return false;
-}, {passive: false});
-function welcome(){
+document.querySelector("#gamingscreen").addEventListener(
+  "wheel",
+  (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    return false;
+  },
+  { passive: false }
+);
+function welcome() {
   begin.play();
 }
 function cargenerator() {
   var i = Math.floor(Math.random() * 10);
   return cars[i];
 }
-function speedduration(){
-    let s = Math.random() * 3;
-    return s+1;
+function speedduration() {
+  let s = Math.random() * 3;
+  return s + 1;
 }
 for (let i = 0; i < 55; i++) {
   count++;
   var car = cargenerator();
-  gamescreen.innerHTML +='<div id="dive' + count + '"><img class="carimages" id="ima'+count+'" src="../Images/' + car + '"></div><div id="div'+ count +'"></div>';
-    var speed = document.getElementById("ima"+count);
-    speed.style.animationDuration = speedduration() + "s";
-
+  gamescreen.innerHTML +=
+    '<div id="dive' +
+    count +
+    '"><img class="carimages" id="ima' +
+    count +
+    '" src="../Images/' +
+    car +
+    '"></div><div id="div' +
+    count +
+    '"></div>';
+  var speed = document.getElementById("ima" + count);
+  speed.style.animationDuration = speedduration() + "s";
 }
 gamescreen.scrollTop = gamescreen.scrollHeight;
 function forwardmovement() {
   gamescreen.scrollTop -= 100;
   if (score >= 500) {
     win.play();
-    gamescreen.innerHTML = '<div id="won">You Won<button id="retrybtn" onclick="relod()">Play Again</button></div>';
-    consolebtn.style.display='none';
-    characterposition.style.display='none';
-    gamescreen.style.height="100vh";
+    gamescreen.innerHTML =
+      '<div id="won">You Won<button id="retrybtn" onclick="relod()">Play Again</button></div>';
+    consolebtn.style.display = "none";
+    characterposition.style.display = "none";
+    gamescreen.style.height = "100vh";
     clearInterval(le);
   }
-  if(flipdiv){
+  if (flipdiv) {
     me = count;
     count--;
-    score=550-(10*count);
-    scorediv.innerHTML=" " +score+"";
+    progress--;
+    score = 550 - 10 * count;
+    scorediv.innerHTML = " " + score + "";
+    progressdiv.innerHTML = progress + " ";
     flipdiv = false;
-    
-  }else{
+  } else {
     me = 90;
     flipdiv = true;
   }
 }
 function Backwardmovement() {
-  if (score != 0){
+  if (score != 0) {
     gamescreen.scrollTop += 100;
-    if(flipdiv){
+    if (flipdiv) {
       me = count;
       flipdiv = false;
-    }else{
+    } else {
       me = 90;
       count++;
-      score=550-(10*count);
-      scorediv.innerHTML=" " +score+"";
+      progress++;
+      progressdiv.innerHTML = progress + " ";
+      score = 550 - 10 * count;
+      scorediv.innerHTML = " " + score + "";
       flipdiv = true;
     }
   }
 }
-function relod(){
+function relod() {
   window.location.reload();
 }
-var le = setInterval(()=>{
-  charpos=parseInt(window.getComputedStyle(characterposition,null).getPropertyValue('left'));
-  y=charpos;
-  if(me!=90){
-    carposition = document.querySelector("#ima"+me);
-    carpos = parseInt(window.getComputedStyle(carposition,null).getPropertyValue('left'));
+var le = setInterval(() => {
+  charpos = parseInt(
+    window.getComputedStyle(characterposition, null).getPropertyValue("left")
+  );
+  y = charpos;
+  if (me != 90) {
+    carposition = document.querySelector("#ima" + me);
+    carpos = parseInt(
+      window.getComputedStyle(carposition, null).getPropertyValue("left")
+    );
     let gap = carpos - charpos;
-    if(gap<70 && gap > -150){
+    if (gap < 70 && gap > -150) {
       clearInterval(le);
       crash.play();
-      gamescreen.innerHTML = '<div id="won">Game Over<button id="retrybtn" onclick="relod()">Retry</button></div>';
-      consolebtn.style.display='none';
-      characterposition.style.display='none';
-      gamescreen.style.height="100vh";
+      gamescreen.innerHTML =
+        '<div id="won">Game Over<button id="retrybtn" onclick="relod()">Retry</button></div>';
+      consolebtn.style.display = "none";
+      characterposition.style.display = "none";
+      gamescreen.style.height = "100vh";
     }
   }
-},50);
-function leftmovement(){
-  if(charpos>50 && charpos < maxwidth){
-    y=y-10;
-    characterposition.style.left=y+"px";
+}, 50);
+function leftmovement() {
+  if (charpos > 50 && charpos < maxwidth) {
+    y = y - 10;
+    characterposition.style.left = y + "px";
   }
 }
-function rightmovement(){
-  if(charpos>30 && charpos< (maxwidth-120)){
-    y=y+10;
-    characterposition.style.left=y+"px";
+function rightmovement() {
+  if (charpos > 30 && charpos < maxwidth - 120) {
+    y = y + 10;
+    characterposition.style.left = y + "px";
   }
 }
